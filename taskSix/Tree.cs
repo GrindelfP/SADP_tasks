f039080adf115b1acea65ade3bf3215a599fb946
Grindelf <grindelf.perlomutrovij@gmail.com>
2022-11-23 17:58:29 +0300

    first working build

--- taskSix/Tree.cs
+++ taskSix/Tree.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace taskSix
 {
@@ -8,10 +6,7 @@ namespace taskSix
     {
         private Node<Movie> _root;
 
-        public Tree()
-        {
-            _root = null;
-        }
+        public Tree() { _root = null; }
 
         public bool Add(Movie data) // works
         {
@@ -37,7 +32,6 @@ namespace taskSix
                         currentNode = currentNode.Left;
                         continue;
                     }
-                    return true; // do we need it?
                 }
                 else
                 {
@@ -51,7 +45,6 @@ namespace taskSix
                         currentNode = currentNode.Right;
                         continue;
                     }
-                    return true; // do we need it?
                 }
             }
             return false;
@@ -59,54 +52,20 @@ namespace taskSix
 
         public Movie Find(int key)
         {
-            List<object> found = InternalSearch(key);
-            if (found == null) return null;
-            Node<Movie> data = found[0] as Node<Movie>;
-            return data.Data;
-        }
-        public bool Remove(int key)
-        {
-            bool rootPositioned = false;
-            List<object> removableAndPrevious = InternalSearch(key);
-            Node<Movie> removable = removableAndPrevious[0] as Node<Movie>;
-            List<object> previousAndInfo = removableAndPrevious[1] as List<object>;
-            Node<Movie> previous = previousAndInfo[0] as Node<Movie>;
-            bool isLeft = previousAndInfo[1] as string == "yes";
-            if (removable == null) return false;
-            if (removable == _root) rootPositioned = true;
-            // for leaf type of the node
-            if (removable.Type == NodeType.LEAF) return LeafRemove(rootPositioned, isLeft, previous); ;
-            // for unary inode type of the node
-            if (removable.Type == NodeType.UNARY_INODE) return UnaryInodeRemove(isLeft, previous, removable);
-            // for binary inode type of the node
-            return BinaryInodeRemove(isLeft, previous, removable);
-        }
-
-        public void Visualize(ListBox listBox)  // works
-        {
-            listBox.Items.Clear();
-            Show(listBox, 1, _root, "");
-        }
-        
-        private List<object> InternalSearch(int key)
-        {
             if (_root == null) return null;
             Node<Movie> currentNode = _root;
-            List<object> previous = null;
             while (currentNode != null)
             {
-                if (currentNode.Data.Duration == key) return new List<object>() { currentNode, previous };
-                if (currentNode.Data.Duration < key)
+                if (currentNode.Data.Duration == key) return currentNode.Data;
+                if (currentNode.Data.Duration > key)
                 {
                     if (currentNode.Left == null) return null;
-                    if (currentNode.Left.Data.Duration == key) previous = new List<object>() { currentNode, "yes" };
                     currentNode = currentNode.Left;
                     continue;
                 }
-                if (currentNode.Data.Duration > key)
+                if (currentNode.Data.Duration < key)
                 {
                     if (currentNode.Right == null) return null;
-                    if (currentNode.Right.Data.Duration == key) previous = new List<object>() { currentNode, null };
                     currentNode = currentNode.Right;
                     continue;
                 }
@@ -114,37 +73,122 @@ namespace taskSix
             return null;
         }
 
+        public bool Remove(int key)
+        {
+            if (_root == null) return false;
+            Node<Movie> removable = null;
+            Node<Movie> previous = null;
+            
+            // search for the removable element
+            bool isLeft = false;
+            bool rootPositioned = false;
+            if (_root.Data.Duration == key) // root fits
+            {
+                removable = _root;
+                rootPositioned = true;
+            } 
+            else // root does not fit
+            {
+                Node<Movie> currentNode = _root;
+                while (currentNode.Left != null || currentNode.Right != null)
+                {
+                    if (currentNode.Left != null && currentNode.Left.Data.Duration == key) 
+                    {
+                        isLeft = true;
+                        removable = currentNode.Left;
+                        previous = currentNode;
+                        break;
+                    } 
+                    if (currentNode.Right != null && currentNode.Right.Data.Duration == key) 
+                    {
+                        removable = currentNode.Right;
+                        previous = currentNode;
+                        break;
+                    } 
+                    if (currentNode.Data.Duration > key)
+                    {
+                        if (currentNode.Left == null) break;
+                        currentNode = currentNode.Left;
+                        continue;
+                    }
+                    if (currentNode.Data.Duration < key)
+                    {
+                        if (currentNode.Right == null) break;
+                        currentNode = currentNode.Right;
+                        continue;
+                    }
+                }
+            }
+            
+            // processing the removable element
+            if (removable == null) return false;           
+            if (removable.Type == NodeType.LEAF) return LeafRemove(rootPositioned, previous, isLeft);
+            if (removable.Type == NodeType.UNARY_INODE) return UnaryInodeRemove(rootPositioned, isLeft, previous, removable);
+            return BinaryInodeRemove(rootPositioned, isLeft, previous, removable);
+        }
+
+        public void Visualize(ListBox listBox) // works
+        {
+            listBox.Items.Clear();
+            Show(listBox, 1, _root, "");
+        }
+        
+
         private void Show(ListBox listBox, int numberOfLevel, Node<Movie> currentNode, string space) // works
         {
             if (_root == null) return;
             string visualization = space;
             visualization += numberOfLevel.ToString() + " " + currentNode.Data.ToString();
             listBox.Items.Add(visualization);
-            //bool noLeft = false;
-            //if (currentNode.Left == null) noLeft = true;
             if (currentNode.Left != null) Show(listBox, numberOfLevel + 1, currentNode.Left, " <-");
-            //if (currentNode.Right == null) return;
             if (currentNode.Right != null) Show(listBox, numberOfLevel + 1, currentNode.Right, " ->");
         }
 
-        private bool LeafRemove(bool rootPositioned, bool isLeft, Node<Movie> previous)
+        private bool LeafRemove(bool rootPositioned, Node<Movie> previous, bool isLeft) // works
         {
             if (rootPositioned) _root = null;
-            if (isLeft) previous.Left = null;
-            previous.Right = null;
+            else
+            {
+                if (isLeft) previous.Left = null;
+                else previous.Right = null;
+            }
             return true;
         }
 
-        private bool UnaryInodeRemove(bool isLeft, Node<Movie> previous, Node<Movie> removable)
+        private bool UnaryInodeRemove(bool rootPositioned, bool isLeft, Node<Movie> previous, Node<Movie> removable) // works
         {
-            if (isLeft) previous.Left = removable.Next();
-            previous.Right = removable.Next();
+            if (rootPositioned)
+            {
+               _root = removable.Next();
+            }
+            else
+            {
+                if (isLeft) previous.Left = removable.Next();
+                else previous.Right = removable.Next();
+            }
             return true;
         }
 
-        private bool BinaryInodeRemove(bool isLeft, Node<Movie> previous, Node<Movie> removable)
+        private bool BinaryInodeRemove(bool rootPositioned, bool isLeft, Node<Movie> previous, Node<Movie> removable)
         {
-            throw new NotImplementedException();
+            Node<Movie> replaceable = removable.Left;
+            while (replaceable.Right != null) { replaceable = replaceable.Right; }
+            Node<Movie> tempor = replaceable;
+            tempor.Right = removable.Right;
+            Remove(replaceable.Data.Duration);
+            if (rootPositioned)
+            {
+                _root = tempor;
+            }
+            else
+            {
+                
+                if (isLeft) previous.Left = tempor;
+                else previous.Right = tempor;
+            }
+            
+            
+            return true;
         }
     }
 }
